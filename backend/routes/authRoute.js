54abd678097cb92322a93830281ad2735a9c4d76
neerajcode34 <109073148+neerajcode34@users.noreply.github.com>
2023-05-11 09:00:24 +0530

    Update authRoute.js

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,3 +1,24 @@
+// const express = require('express');
+// const authController = require('../controllers/authController');
+
+// // check user is logged in or not middleware
+// const { isLoggedIn } = require('../middlewares/userAuth');
+
+// // data validation schema and middlewares
+// const {loginSchema, registerSchema, validator} = require('../middlewares/userValidator');
+
+// const router = express.Router();
+
+// router
+//   .post('/login', validator(loginSchema), authController.login)
+//   .post('/register', validator(registerSchema), authController.register)
+//   .get('/logout', authController.logout)
+//   .get('/isloggedin', isLoggedIn);
+
+
+
+// module.exports = router;
+
 const express = require('express');
 const authController = require('../controllers/authController');
 
@@ -9,11 +30,18 @@ const {loginSchema, registerSchema, validator} = require('../middlewares/userVal
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // set the 'Access-Control-Allow-Origin' header to allow requests from http://localhost:3000
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST'); // set the allowed HTTP methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // set the allowed request headers
+  res.setHeader('Access-Control-Allow-Credentials', 'true'); // set the 'Access-Control-Allow-Credentials' header to 'true'
+  next();
+});
+
 router
   .post('/login', validator(loginSchema), authController.login)
   .post('/register', validator(registerSchema), authController.register)
   .get('/logout', authController.logout)
   .get('/isloggedin', isLoggedIn);
-
 
 module.exports = router;
