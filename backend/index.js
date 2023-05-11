cc5dab6766f37d34e4e31e443aae71144a71f262
neerajcode34 <109073148+neerajcode34@users.noreply.github.com>
2023-05-11 08:44:06 +0530

    Update index.js again

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,18 @@ const app = express();
 
 app.use(cors()); 
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", "true"); // set the 'Access-Control-Allow-Credentials' header to 'true'
+  next();
+});
+
+
+
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
